6c48ffc0b845db25023b3ceba53e5c2d17ebc074
mdsk42694-hue <mdsk42694@gmail.com>
2026-07-20 20:24:41 +0600

    Add configuration variables for social media links

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "8752332500:AAGQowBT2MM2Uri3QtoPRQkTvIc0oJMU0bY"
+
+FACEBOOK_LINK = "https://www.facebook.com/share/1LxuVGHEMz/"
+YOUTUBE_LINK = "https://www.youtube.com/@khlomni7_aman_ullah"
+TIKTOK_LINK = "tiktok.com/@khl.omni7.aman.ul"
+TELEGRAM_LINK = "https://t.me/khlomni7amanullah"
+
+YOUTUBE_KEYWORDS = ["KHL Omni7 aman ullah", "Subscribed"]
+FACEBOOK_KEYWORDS = ["KHL Omni7 aman ullah", "Following"]
+
+ACCESS_DURATION = 86400
